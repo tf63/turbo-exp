23fee6c9a347062d664957b0f36338602a394e1e
tf63 <74246282+tf63@users.noreply.github.com>
2024-07-30 21:02:58 +0900

    feat: update tailwind config

--- apps/react/tailwind.config.ts
+++ apps/react/tailwind.config.ts
@@ -2,7 +2,7 @@ import sharedConfig from '@repo/config-tailwind/tailwind'
 import type { Config } from 'tailwindcss'
 
 const config: Pick<Config, 'content' | 'presets'> = {
-    content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'],
+    content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
     presets: [sharedConfig],
 }
 
